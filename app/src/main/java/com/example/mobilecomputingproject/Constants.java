36b008825a58bba3f2e0a6fa06392a59c3a213ef
Hamza Najib <hamzan47@gmail.com>
2021-06-11 23:38:53 +0500

    first commit

---
+++ app/src/main/java/com/example/mobilecomputingproject/Constants.java
@@ -0,0 +1,6 @@
+package com.example.mobilecomputingproject;
+
+public class Constants {
+
+    public static final int ERROR_DIALOG_REQUEST = 9001;
+}
---
+++ app/src/main/java/com/example/mobilecomputingproject/UserClient.java
@@ -0,0 +1,19 @@
+package com.example.mobilecomputingproject;
+
+import android.app.Application;
+
+import com.example.mobilecomputingproject.models.User;
+
+public class UserClient extends Application {
+
+    private User user = null;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+}
